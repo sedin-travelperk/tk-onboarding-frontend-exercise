dac4014fa88d467c340744ee29c0ca5808ec287c
sedin-travelperk <edin.sehovic@travelperk.com>
2021-12-16 16:00:20 +0100

    redirect / to /recipes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import RecipeListScreen from "./recipe/screens/RecipeListScreen";
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, Navigate} from "react-router-dom";
 import RecipeScreen from "./recipe/screens/RecipeScreen";
 
 function App() {
   return (
     <div >
         <Routes>
-                <Route exact path="/recipes" element={<RecipeListScreen/>}/>
-                <Route exact path="/recipes/:recipeId" element={<RecipeScreen/>}/>
+            <Route exact path="/" element={<Navigate to="/recipes" />}/>
+            <Route exact path="/recipes" element={<RecipeListScreen/>}/>
+            <Route exact path="/recipes/:recipeId" element={<RecipeScreen/>}/>
         </Routes>
     </div>
   );
 }
+
 
 export default App;
